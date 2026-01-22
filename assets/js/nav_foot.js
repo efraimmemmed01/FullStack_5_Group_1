e9871a12f7aa5a0ac714e6a0401be131647c848f
JafarTheAlien <cefertarverdiyevv@gmail.com>
2026-01-22 20:00:55 +0400

    Demo with Added features: Responsible desg, Finished Footer

--- assets/js/nav_foot.js
+++ assets/js/nav_foot.js
@@ -1,2 +1,44 @@
 let body = document.body
 
+
+// Bura linklere daxil olanda etraflarini deyisen linkler ucun koddur. 
+let currentPath_with_slash = window.location.pathname
+
+let currentPath = currentPath_with_slash.slice(1, currentPath_with_slash.length)
+
+console.log(currentPath);
+
+let a = document.querySelectorAll(".nav-link")
+
+a.forEach((x) => {
+    console.log(x.getAttribute("href"));
+
+    if (x.getAttribute("href") == currentPath) {
+        x.classList.add("active-link")
+        console.log("we are here");
+
+    }
+}
+);
+
+
+// booking button
+let down_items_div = document.querySelector(".down-items")
+let btn_book = down_items_div.querySelector("button")
+
+btn_book.addEventListener("click", () => {
+
+    setTimeout(() => {
+        window.open("book.html", "_self")
+
+    },100)
+
+})
+btn_book.addEventListener("mousedown", () => {
+    btn_book.classList.toggle("btn-color-on-click")
+})
+btn_book.addEventListener("mouseup", () => {
+    btn_book.classList.toggle("btn-color-on-click")
+
+})
+
